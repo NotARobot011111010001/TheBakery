f5dc10b92fa08b8417c935e0140824da5812e162
Jack <griff.jack@gmail.com>
2022-05-14 16:48:45 +0100

    Added bottom nav bar functionality

--- app/src/main/java/com/example/shoppr/HomeActivity.kt
+++ app/src/main/java/com/example/shoppr/HomeActivity.kt
@@ -2,6 +2,13 @@ package com.example.shoppr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 
 
@@ -9,5 +16,13 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_bar)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_host_frag) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        bottomNavigationView.setupWithNavController(navController)
+
+
     }
 }
