6e540977e213e553ea124b6514e10261a6a7a6d0
Jack <griff.jack@gmail.com>
2022-05-14 15:31:36 +0100

    Added drawable assets Added google play services api Added 4 base fragments for navigation bar to swap between Implemented bottom nav bar with updated icons and animations when selected

--- app/src/main/java/com/example/shoppr/MainActivity.kt
+++ app/src/main/java/com/example/shoppr/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.shoppr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun goHome(view: View){
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/shoppr/ShoppingItems.kt
@@ -0,0 +1,3 @@
+package com.example.shoppr
+
+data class ShoppingItems (val name: String, val desc : String, val price : Float, val Image : Int)
