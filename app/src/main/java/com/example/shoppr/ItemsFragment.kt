c14b88899fdefe245f7fcd39901a601dfc8e9504
Jack <griff.jack@gmail.com>
2022-05-14 20:52:59 +0100

    Cleaned up formatting of grid items Cleared list of grid items when view changes Added extra dummy data to test with

--- app/src/main/java/com/example/shoppr/ItemsFragment.kt
+++ app/src/main/java/com/example/shoppr/ItemsFragment.kt
@@ -2,6 +2,7 @@ package com.example.shoppr
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,6 +16,7 @@ import android.widget.TextView
 data class ShoppingItems (
     val name: String,
     val price : Double,
+    val weight: String,
     val Image : Int)
 
 class ItemsFragment : Fragment() {
@@ -22,6 +24,7 @@ class ItemsFragment : Fragment() {
 
     private val images: IntArray = intArrayOf(R.drawable.bread, R.drawable.carrots,R.drawable.sandwich,R.drawable.wine)
     private val names = arrayOf("Bread", "Carrots", "Sandwich", "Wine")
+    private val weights = arrayOf("500g", "500g", "120g", "750ml")
     private val prices = arrayOf(4.99, 2.45, 4.25, 10.99)
 
     private var gridAdapter: GridAdapater? = null
@@ -40,14 +43,25 @@ class ItemsFragment : Fragment() {
 
         gridView = view.findViewById(R.id.items_grid)
 
-        for(index in names.indices){
-            val item = ShoppingItems(names[index], prices[index], images[index])
-            shoppingItemsList.add(item)
+
+        for(extra in 0..5) {
+            for (index in names.indices) {
+                val item = ShoppingItems(names[index], prices[index], weights[index], images[index])
+                shoppingItemsList.add(item)
+            }
         }
+
         gridAdapter = GridAdapater(context,shoppingItemsList)
         gridView.adapter = gridAdapter
 
     }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        shoppingItemsList.clear()
+    }
+
 
     class GridAdapater(private val context: Context?, private val gridItems: MutableList<ShoppingItems>) : BaseAdapter() {
 
@@ -69,10 +83,12 @@ class ItemsFragment : Fragment() {
             val image : ImageView = convertView.findViewById(R.id.item_image)
             val name : TextView = convertView.findViewById(R.id.item_name)
             val price : TextView = convertView.findViewById(R.id.item_price)
+            val weight : TextView = convertView.findViewById(R.id.item_weight)
 
             image.setImageResource(filteredGridItems[position].Image)
             name.text = filteredGridItems[position].name
-            price.text = filteredGridItems[position].price.toString()
+            price.text = "£" + filteredGridItems[position].price.toString()
+            weight.text = filteredGridItems[position].weight
 
             return convertView
         }
